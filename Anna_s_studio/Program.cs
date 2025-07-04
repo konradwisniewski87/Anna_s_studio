df383320775e47596061d49966d01595bc4cac58
Konrad Wiśniewski <konrad.wisniewski87@gmail.com>
2025-07-05 00:32:51 +0200

    Added NavMenu i translations

--- Anna_s_studio/Program.cs
+++ Anna_s_studio/Program.cs
@@ -1,10 +1,12 @@
 using Anna_s_studio;
+using Anna_s_studio.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddSingleton<I18nService>();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
---
+++ Anna_s_studio/Services/I18nService.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.Json;
+
+namespace Anna_s_studio.Services;
+
+public class I18nService
+{
+    public event Action OnChange;
+    private Dictionary<string, Dictionary<string, string>> _translations;
+    public string CurrentLanguage { get; private set; } = "pl";
+
+    public async Task LoadAsync(HttpClient http)
+    {
+        var json = await http.GetStringAsync("i18n.json");
+        _translations = JsonSerializer.Deserialize<Dictionary<string, Dictionary<string, string>>>(json)!;
+    }
+
+    public void SetLanguage(string lang)
+    {
+        CurrentLanguage = lang;
+        OnChange?.Invoke();
+    }
+
+    public string T(string key)
+    {
+        if (_translations != null && _translations.TryGetValue(CurrentLanguage, out var dict) && dict.TryGetValue(key, out var value))
+        {
+            return value;
+        }
+        return key;
+    }
+}
