31b58f945dd690d5a1b9b9b9770839ea2b20b79d
Konrad Wiśniewski <konrad.wisniewski87@gmail.com>
2025-07-08 13:24:35 +0200

    Added visit booking

---
+++ Anna_s_studio/Services/VisitBookingService.cs
@@ -0,0 +1,38 @@
+﻿namespace Anna_s_studio.Services;
+
+public class VisitBookingService
+{
+    public string GetGoogleCalendarUrl(DateTime start, DateTime end, string title, string location, string details)
+    {
+        string dateFormat = "yyyyMMddTHHmmss";
+        string startUtc = start.ToString(dateFormat);
+        string endUtc = end.ToString(dateFormat);
+
+        var url = $"https://calendar.google.com/calendar/render?action=TEMPLATE" +
+                  $"&text={Uri.EscapeDataString(title)}" +
+                  $"&dates={startUtc}/{endUtc}" +
+                  $"&details={Uri.EscapeDataString(details)}" +
+                  $"&location={Uri.EscapeDataString(location)}" +
+                  $"&ctz=Europe/Warsaw";
+        return url;
+    }
+
+    public string GetIcsCalendarUrl(DateTime start, DateTime end, string title, string location, string details)
+    {
+        var dateFormat = "yyyyMMddTHHmmss";
+        string ics =
+            $"BEGIN:VCALENDAR\n" +
+            $"VERSION:2.0\n" +
+            $"BEGIN:VEVENT\n" +
+            $"SUMMARY:{title}\n" +
+            $"DTSTART:{start.ToString(dateFormat)}\n" +
+            $"DTEND:{end.ToString(dateFormat)}\n" +
+            $"LOCATION:{location}\n" +
+            $"DESCRIPTION:{details}\n" +
+            $"END:VEVENT\n" +
+            $"END:VCALENDAR";
+        var base64 = Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(ics));
+        return $"data:text/calendar;charset=utf-8;base64,{base64}";
+    }
+}
+
