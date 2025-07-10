e59d985b8b09ec19d309929b3c16434a4e62270d
Konrad Wiśniewski <konrad.wisniewski87@gmail.com>
2025-07-10 14:49:04 +0200

    Services.razor looks like good

---
+++ Anna_s_studio/Services/Service.cs
@@ -0,0 +1,10 @@
+namespace Anna_s_studio.Services
+{
+    public class Service
+    {
+        public string Emoji { get; set; }
+        public string Name { get; set; }
+        public int Duration { get; set; }
+        public string Price { get; set; }
+    }
+} 
